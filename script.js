b7309c391521cb99dbe3411876aa85224ab39c21
KullerCode <kullerblue@icloud.com>
2025-04-25 20:20:01 -0400

    forward progress

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+  document.getElementById('hamburger').addEventListener('click', function () {
+    const menu = document.getElementById('nav-menu');
+    menu.classList.toggle('show');
+  });
+
+document.getElementById('hamburger').addEventListener('click', function () {
+    document.getElementById('nav-menu').classList.toggle('show'); });
+</script>
