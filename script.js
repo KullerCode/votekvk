ff719b7f4cc8e3506dbab101aebc6c8e2b1018b3
KullerCode <kullerblue@icloud.com>
2025-04-30 23:01:45 -0400

    swapped JS script for simpler version

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 <script>
   document.getElementById('hamburger').addEventListener('click', function () {
-    const menu = document.getElementById('nav-menu');
-    menu.classList.toggle('show');
+    document.getElementById('nav-menu').classList.toggle('show');
   });
-
-document.getElementById('hamburger').addEventListener('click', function () {
-    document.getElementById('nav-menu').classList.toggle('show'); });
 </script>
